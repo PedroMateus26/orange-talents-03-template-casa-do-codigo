caacee8e677ad5ef80b204fa4e1daeb0fea56e6e
PedroMateus26 <pedromateusneto@gmail.com>
2021-03-19 08:31:19 -0300

    setup do projeto

---
+++ src/test/java/br/zupacademy/pedro/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.zupacademy.pedro.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
